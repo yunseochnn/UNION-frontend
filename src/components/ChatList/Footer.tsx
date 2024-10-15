244773b87351ececa9b09ddd385f8d4b30303b50
qowldud <bjy6631@daum.net>
2024-10-15 17:14:37 +0900

    style: footer 스타일 변경

--- src/components/ChatList/Footer.tsx
+++ src/components/ChatList/Footer.tsx
@@ -2,7 +2,7 @@ import SideBar from '../../common/SideBar';
 
 const Footer = () => {
   return (
-    <div className="w-full h-14 flex items-center justify-center">
+    <div className="w-full h-14 flex justify-center">
       <SideBar />
     </div>
   );
