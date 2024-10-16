32c35f3c5903c63ee8a03ca9715491983b60d7fc
qowldud <bjy6631@daum.net>
2024-10-16 12:26:56 +0900

    feat: 모임 글쓰기 날짜, 인원수 설정 화면 구현

--- src/components/MeetWrite/Content.tsx
+++ src/components/MeetWrite/Content.tsx
@@ -2,16 +2,20 @@ import Policy from '../../common/Policy';
 import Calendar from './Calendar';
 import '../../style.css';
 import People from './People';
+import { IoPeople } from 'react-icons/io5';
 
 const Content = () => {
   return (
     <div className="flex flex-col flex-1 overflow-y-auto hidden-scrollbar mt-4">
-      <div className="font-bold">시간 인원 설정</div>
+      <div className="font-bold ml-2">시간 인원 설정</div>
       <div className="flex mb-3 justify-between items-center">
         <div>
           <Calendar />
         </div>
-        <div>
+        <div className="flex items-center">
+          <div>
+            <IoPeople size={20} />
+          </div>
           <People />
         </div>
       </div>
