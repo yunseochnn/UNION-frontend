e56b60d680a0a9e68388bff04868151dc63aff9d
qowldud <bjy6631@daum.net>
2024-10-18 11:15:39 +0900

    feat: chat 필터링 수정

--- src/components/ChatList/Lists.tsx
+++ src/components/ChatList/Lists.tsx
@@ -3,20 +3,20 @@ import '../../style.css';
 import List from './List';
 
 const Lists = () => {
-  const [isGroup, setIsGroup] = useState(false);
+  const [filter, setFilter] = useState('개인');
 
   return (
     <div className="flex flex-col flex-1 overflow-y-auto hidden-scrollbar mt-5">
       <div className="flex gap-3 font-semibold">
         <span
-          className={`cursor-pointer ${isGroup ? 'text-gray-400' : 'text-black'}`}
-          onClick={() => setIsGroup(!isGroup)}
+          className={`cursor-pointer ${filter === '모임' ? 'text-gray-400' : 'text-black'}`}
+          onClick={() => setFilter('개인')}
         >
           개인
         </span>
         <span
-          className={`cursor-pointer ${isGroup ? 'text-black' : 'text-gray-400'}`}
-          onClick={() => setIsGroup(!isGroup)}
+          className={`cursor-pointer ${filter === '모임' ? 'text-black' : 'text-gray-400'}`}
+          onClick={() => setFilter('모임')}
         >
           모임
         </span>
