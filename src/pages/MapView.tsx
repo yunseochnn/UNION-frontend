99ff89a99b3a69dedba6868b436736eb1bb985bc
qowldud <bjy6631@daum.net>
2024-10-29 16:24:06 +0900

    feat: 지도뷰 모임 모달창 ui 구현

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,11 +1,13 @@
 import Content from '../components/MapView/Content';
 import Header from '../components/MapView/Header';
+import MeetModal from '../components/MapView/MeetModal';
 
 export default function MapView() {
   return (
-    <div className="flex flex-col w-full h-full py-3">
+    <div className="flex flex-col w-full h-full py-3 relative">
       <Header />
       <Content />
+      <MeetModal />
     </div>
   );
 }
