53da26561edfd69b23ae2ff833f4054223d1249d
BUMSU KIM <kbsbmori3@gmail.com>
2024-11-03 14:54:51 +0900

    fix:라우팅 경로 모든 글자 대문자로 수정

--- src/api/BoardListRequest.ts
+++ src/api/BoardListRequest.ts
@@ -61,11 +61,10 @@ export const fetchBoardPosts = async ({
   page = 0,
   size = 3,
 }: ReadBoardRequestParams): Promise<BoardListResponse> => {
-  const url = `/Board/${boardType}`;
+  const url = `/board/${boardType.toUpperCase()}`; // 소문자 board로 변경, type은 대문자로 변환
 
   try {
-    const response = await apiClient.get<BoardListResponse>(url, {
-      headers: {
+    const response = await apiClient.get<BoardListResponse>(url, {      headers: {
         Authorization: Cookies.get('Authorization'),
       },
       params: {
