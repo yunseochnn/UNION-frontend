3e37205c111983dc8da2822c58cad5eb08537b36
qowldud <bjy6631@daum.net>
2024-11-05 11:32:46 +0900

    feat: 지도뷰 마커 찍기 기능 구현

--- src/components/MapView/MeetModal.tsx
+++ src/components/MapView/MeetModal.tsx
@@ -1,7 +1,13 @@
 import { BsFillPeopleFill } from 'react-icons/bs';
 import { IoMdEye } from 'react-icons/io';
+import { List } from '../../pages/MapView';
+import dayjs from 'dayjs';
 
-const MeetModal = () => {
+interface Prop {
+  modalContent: List;
+}
+
+const MeetModal = ({ modalContent }: Prop) => {
   return (
     <div className="absolute w-full h-[150px] bg-white bottom-0 z-10 flex items-center justify-center">
       <div className="w-[85%] flex justify-between">
@@ -9,14 +15,14 @@ const MeetModal = () => {
           <div className="flex">
             <div className="w-5 h-5 bg-gray-300 rounded-md mr-1"></div>
             <div className="font-medium text-sm">
-              <span style={{ color: '#4D5159' }}>닉네임 · </span>
-              <span style={{ color: '#868B94' }}> 상도동</span>
+              <span style={{ color: '#4D5159' }}>{modalContent.author.nickname} · </span>
+              <span style={{ color: '#868B94' }}> {modalContent.eupMyeonDong}</span>
             </div>
           </div>
           <div>
-            <div className="font-bold mt-3 text-base">휘겸재 전시&카페</div>
+            <div className="font-bold mt-3 text-base">{modalContent.title}</div>
             <div className="font-semibold text-sm" style={{ color: '#868B94' }}>
-              오늘, 오후 4:00
+              {dayjs(modalContent.gatheringDateTime).format('MM월 DD일 A H:mm')}
             </div>
           </div>
           <div className="flex gap-2 mt-1">
@@ -24,14 +30,14 @@ const MeetModal = () => {
               <span>
                 <BsFillPeopleFill />
               </span>
-              <span>1/4명</span>
+              <span>{`${modalContent.currentMember}/${modalContent.maxMember}명`}</span>
             </div>
 
             <div className="flex gap-1 items-center text-gray-500 text-sm">
               <span>
                 <IoMdEye />
               </span>
-              <span>24</span>
+              <span>{modalContent.views}</span>
             </div>
           </div>
         </div>
--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
 import Content from '../components/MapView/Content';
 import Header from '../components/MapView/Header';
 import MeetModal from '../components/MapView/MeetModal';
 
+export interface List {
+  id: number;
+  title: string;
+  maxMember: number;
+  currentMember: number;
+  eupMyeonDong: string;
+  gatheringDateTime: string;
+  views: number;
+  latitude: number;
+  longitude: number;
+  author: {
+    token: string;
+    nickname: string;
+    profileImage: string;
+    univName: string;
+  };
+  thumbnail: string | null;
+}
+
 export default function MapView() {
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const [modalContent, setModalContent] = useState<List | null>(null);
+  console.log(modalContent);
   return (
     <div className="flex flex-col w-full h-full py-3 relative">
       <Header />
-      <Content />
-      <MeetModal />
+      <Content
+        latitude={latitude}
+        setLatitude={setLatitude}
+        longitude={longitude}
+        setLongitude={setLongitude}
+        setModalContent={setModalContent}
+      />
+      {modalContent && <MeetModal modalContent={modalContent} />}
     </div>
   );
 }
