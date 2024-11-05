9b9673e2b32587709a01ff50025739c80aab9db2
qowldud <bjy6631@daum.net>
2024-11-05 15:53:30 +0900

    design: 게시판 리스트 헤더 수정

--- src/components/Board/Header.tsx
+++ src/components/Board/Header.tsx
@@ -1,8 +1,8 @@
 // components/Board/Header.tsx
 import React from 'react';
-import { IoChevronBack } from 'react-icons/io5';
 import { FiSearch, FiBell } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
 
 interface HeaderProps {
   title: string;
@@ -12,9 +12,9 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   const navigate = useNavigate();
 
   return (
-    <header className="flex justify-between items-center p-4">
+    <header className="flex justify-between items-center px-5 h-[62px]">
       <div className="flex items-center flex-1">
-        <IoChevronBack size={24} className="cursor-pointer" onClick={() => navigate('/board')} />
+        <IoIosArrowBack size={32} className="cursor-pointer" onClick={() => navigate('/board')} />
       </div>
       <h1 className="flex-1 text-center text-lg font-semibold">{title}</h1>
       <div className="flex items-center gap-4 flex-1 justify-end">
