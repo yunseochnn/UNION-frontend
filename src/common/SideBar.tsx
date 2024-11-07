47ef0acfd5f899818423ee3eda00be9c95d9df06
qowldud <bjy6631@daum.net>
2024-11-07 13:27:56 +0900

    design: 네브바 디자인 변경

--- src/common/SideBar.tsx
+++ src/common/SideBar.tsx
@@ -1,10 +1,11 @@
-import { FaRegUser, FaUser } from 'react-icons/fa6';
 import { HiOutlinePencilSquare, HiPencilSquare } from 'react-icons/hi2';
 import { IoChatbubbles, IoChatbubblesOutline, IoHome } from 'react-icons/io5';
 import { useNavigate } from 'react-router-dom';
 import HomeOutline from '/sidebar/home-outline.svg';
 import MeetOuline from '/sidebar/location-outline.svg';
 import FillOutline from '/sidebar/Vector.svg';
+import PersonOutline from '/sidebar/person-outline.svg';
+import PersonFill from '/sidebar/People.svg';
 
 const SideBar = () => {
   const navigate = useNavigate();
@@ -36,9 +37,9 @@ const SideBar = () => {
         <div className="flex flex-col justify-center items-center cursor-pointer" onClick={() => navigate('/meet')}>
           <div>
             {pathname.includes('meet') ? (
-              <img src={FillOutline} width={23} height={23} />
+              <img src={FillOutline} width={15} height={15} />
             ) : (
-              <img src={MeetOuline} width={23} height={23} />
+              <img src={MeetOuline} width={25} height={25} />
             )}
           </div>
           <div className="text-xs">모임</div>
@@ -54,7 +55,13 @@ const SideBar = () => {
 
         <div className="flex flex-col justify-center items-center cursor-pointer " onClick={() => navigate('/myPage')}>
           <div>
-            <div>{pathname.includes('myPage') ? <FaUser size={20} /> : <FaRegUser size={20} />}</div>
+            <div>
+              {pathname.includes('myPage') ? (
+                <img src={PersonFill} width={23} height={23} />
+              ) : (
+                <img src={PersonOutline} width={23} height={23} />
+              )}
+            </div>
           </div>
           <div className="text-xs">마이페이지</div>
         </div>
