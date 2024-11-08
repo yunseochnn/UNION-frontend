87ad0d2f4e98ee58a76df46e330dfe00b8333add
qowldud <bjy6631@daum.net>
2024-11-09 00:02:56 +0900

    design: 모임 기본 프로필

--- src/components/ChatList/List.tsx
+++ src/components/ChatList/List.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { IFChat } from './Lists';
 import dayjs from 'dayjs';
+import { FaUsers } from 'react-icons/fa6';
 
 interface Prop {
   chat: IFChat;
@@ -20,16 +21,24 @@ const List = ({ chat }: Prop) => {
 
   return (
     <div className="w-full h-16 flex items-center mt-2 gap-4 cursor-pointer" onClick={onClickChat}>
-      <div className="w-14 h-14 rounded-full bg-gray-300 flex-shrink-0">
-        {chat.profileImage && <img src={chat.profileImage} />}
+      <div className="w-14 h-14 rounded-full bg-customGray2  flex-shrink-0 flex justify-center items-center overflow-hidden ">
+        {chat.profileImage ? (
+          <img src={chat.profileImage} />
+        ) : (
+          <div>
+            <FaUsers size={30} color="white" />
+          </div>
+        )}
       </div>
 
       <div className="flex flex-col">
         <div className="flex gap-3 items-center">
           <div className="text-base font-bold">{chat.title}</div>
-          <div className="text-sm text-customGray2 font-semibold">
-            {dayjs(chat.createdAt).format('MM월 DD일 A H:mm')}
-          </div>
+          {chat.createdAt && (
+            <div className="text-sm text-customGray2 font-semibold">
+              {dayjs(chat.createdAt).format('MM월 DD일 A H:mm')}
+            </div>
+          )}
         </div>
         <div className="text-sm text-customGray2 font-semibold">
           {chat.content.length > 23 ? chat.content.slice(0, 22) + '...' : chat.content}
