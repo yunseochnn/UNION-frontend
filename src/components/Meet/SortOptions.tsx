f58d963dfb0e5f7d0c0b765d135af9a4a55e99a6
yunseochnn <10cys10@gmail.com>
2024-11-11 13:45:06 +0900

    design: 모임 sort 조정

--- src/components/Meet/SortOptions.tsx
+++ src/components/Meet/SortOptions.tsx
@@ -16,20 +16,20 @@ const SortOptions: React.FC<SortOptionsProps> = ({ sortBy, setSortBy }) => {
   const [showSortOptions, setShowSortOptions] = useState(false);
 
   return (
-    <div className="flex flex-wrap items-center gap-4 px-4 pb-2 text-sm text-gray-600 border-b relative">
+    <div className="flex flex-wrap items-center gap-4 px-4 pb-2 text-sm text-gray-600  relative">
       <div className="relative">
         <button
-          className="w-30 flex items-center justify-center gap-2 border border-gray-300 rounded-full px-3 py-1"
+          className="w-full flex items-center justify-start gap-2 border border-gray-300 rounded-full px-3 py-1"
           onClick={() => setShowSortOptions(!showSortOptions)}
         >
-          <span>{sortByText[sortBy]}</span>
+          <span className="text-left w-full">{sortByText[sortBy]}</span>
           <span>
             <IoIosArrowDown size={15} />
           </span>
         </button>
 
         {showSortOptions && (
-          <div className="absolute top-[calc(100%+1px)] left-0 z-10 w-28 bg-white border border-gray-300 rounded-md shadow-lg">
+          <div className="absolute top-[calc(100%+1px)] left-0 z-10 w-full bg-white border border-gray-300 rounded-md shadow-lg">
             <button
               className="block w-full text-left px-4 py-2 hover:bg-gray-100"
               onClick={() => {
