3f9c3ce4946a774dbb0f7761b1b6f2b2d7c61844
yunseochnn <10cys10@gmail.com>
2024-11-11 13:23:27 +0900

    design: 회색 변경

--- src/components/EmailVerification/Button.tsx
+++ src/components/EmailVerification/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function Button({ onClick, disabled, label }: ButtonProps) {
   return (
     <button
-      className={`text-[23px] font-semibold w-full h-[63px] text-white p-2 rounded-md ${disabled ? 'bg-gray-400' : 'bg-mainColor'}`}
+      className={`text-[23px] font-semibold w-full h-[63px] text-white p-2 rounded-md ${disabled ? 'bg-customGray2' : 'bg-mainColor'}`}
       onClick={onClick}
       disabled={disabled}
     >
